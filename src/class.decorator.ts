924367bb5a8180e816c476ba452c56585316d91e
Alfonso Gomez <stein.hakase.vs@gmail.com>
2022-01-28 02:33:34 -0600

    completed example

--- src/class.decorator.ts
+++ src/class.decorator.ts
@@ -1,14 +1,27 @@
+type Constructor = { new (...arg: any[]): {} }
+
 enum Url {
   PROD = 'production URL',
   DEV = 'development URL'
 }
 
-class FetchAnimals {
-  url: Url
+function ChooseEnvironment<T extends Constructor> (target: T) {
+  const url = process.env.NODE_ENV === 'production'
+    ? Url.PROD
+    : Url.DEV
 
-  constructor (url) {
-    this.url = url
+  return class extends target {
+    url = url
+
+    fetchCats () {
+      console.log(`We just changed the behavior of this method`)
+    }
   }
+}
+
+@ChooseEnvironment
+class FetchAnimals {
+  url?: Url
 
   fetchDogs () {
     console.log(`fetching dogs to url ${this.url}`)
@@ -20,6 +33,6 @@ class FetchAnimals {
 
 }
 
-const imagesFetcher = new FetchAnimals(Url.DEV)
+const imagesFetcher = new FetchAnimals()
 imagesFetcher.fetchCats()
 imagesFetcher.fetchDogs()
